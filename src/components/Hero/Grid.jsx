66a93819de1d0ee40c3dd0553dcdfb646798c6ce
Tom Okafor <tmeka008@gmail.com>
2024-12-31 11:33:13 +0100

    Add responsiveness to hero section.

--- src/components/Hero/Grid.jsx
+++ src/components/Hero/Grid.jsx
@@ -3,14 +3,8 @@ import Image from "../Image";
 export default function Grid() {
   return (
     <div className="heroGrid">
-      <Image
-        source="../../public/assets/img4.png"
-        altText="pineapple on a phone screen"
-      />
-      <Image
-        source="../../public/assets/img5.png"
-        altText="cherry on a phone screen"
-      />
+      <Image source="/assets/img4.png" altText="pineapple on a phone screen" />
+      <Image source="/assets/img5.png" altText="cherry on a phone screen" />
     </div>
   );
 }
